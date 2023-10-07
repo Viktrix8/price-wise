4314c4ea34f9945c408fb2aa4e996ded04f7d09c
Viktor Sopko <sopko.viktor88@gmail.com>
2023-10-07 13:12:44 +0200

    Add email notifications.

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server"
 import { prisma } from "@/lib/db"
 import { scrapeProduct } from "@/lib/scraper"
+import { Product } from "@prisma/client"
+import { createTransport } from "nodemailer"
 
 export const POST = async (req: NextRequest) => {
     try {
@@ -11,45 +13,82 @@ export const POST = async (req: NextRequest) => {
 
         const products = await prisma.product.findMany()
         await Promise.all(products.map(async product => {
-            const newProduct = await scrapeProduct(product.url)
+            // Scrape product and compare the product price with the current price as well as the stock status, if there is a change, update the product and log out the email subscribers
+            const scrapedProduct = await scrapeProduct(product.url)
 
-            if (!newProduct) return
+            if (!scrapedProduct) return
+            const newProduct: Partial<Product> = {}
 
-            if (product.highestPrice < newProduct.price) {
+            if (scrapedProduct.price !== product.price) {
+                newProduct.price = scrapedProduct.price
+            }
+
+            if (scrapedProduct.stockStatus !== product.stockStatus) {
+                newProduct.stockStatus = scrapedProduct.stockStatus
+            }
+
+            if (scrapedProduct.isInSale !== product.isInSale) {
+                newProduct.isInSale = scrapedProduct.isInSale
+            }
+
+            if (scrapedProduct.originalPrice !== product.originalPrice) {
+                newProduct.originalPrice = scrapedProduct.originalPrice
+            }
+
+            if (scrapedProduct.rating !== product.rating) {
+                newProduct.rating = scrapedProduct.rating
+            }
+
+            if (scrapedProduct.ratingCount !== product.ratingCount) {
+                newProduct.ratingCount = scrapedProduct.ratingCount
+            }
+
+            const scrappedPrice = scrapedProduct.price
+            if (scrappedPrice > product.highestPrice) {
+                newProduct.highestPrice = scrappedPrice;
+            }
+            if (scrappedPrice < product.lowestPrice) {
+                newProduct.lowestPrice = scrappedPrice;
+            }
+
+            if (Object.keys(newProduct).length > 0) {
                 await prisma.product.update({
                     where: {
                         id: product.id
                     },
-                    data: {
-                        highestPrice: newProduct.price,
-                        price: newProduct.price,
-                    }
-                })
-            }
-            if (product.lowestPrice > newProduct.price) {
-                await prisma.product.update({
-                    where: {
-                        id: product.id
-                    },
-                    data: {
-                        lowestPrice: newProduct.price,
-                        price: newProduct.price,
-                    }
+                    data: newProduct
                 })
             }
 
-            await prisma.product.update({
+            const subscribers = await prisma.subscription.findMany({
                 where: {
-                    id: product.id
-                },
-                data: {
-                    stockStatus: newProduct.stockStatus,
-                    isInSale: newProduct.isInSale,
-                    originalPrice: newProduct.originalPrice,
-                    rating: newProduct.rating,
-                    ratingCount: newProduct.ratingCount,
+                    productId: product.id
                 }
             })
+
+            if (subscribers.length) {
+                await Promise.all(subscribers.map(async subscriber => {
+                    const transporter = createTransport({
+                        service: "gmail",
+                        host: "smtp.gmail.com",
+                        auth: {
+                            user: "najetak.viktrixeu@gmail.com",
+                            pass: process.env.GMAIL_PASSWORD
+                        }
+                    })
+
+                    await transporter.sendMail({
+                        from: "najetak.viktrixeu@gmail.com",
+                        to: subscriber.email,
+                        subject: "Product price change",
+                        html: `
+                            <h1>Product price change</h1>
+                            <p>Product: ${product.title}</p>
+                            <a href="${`${process.env.PUBLIC_URL}/products/${product.id}`}">Product link</a>
+                        `
+                    })
+                }))
+            }
         }))
 
         return new Response("Ok", { status: 200 })
